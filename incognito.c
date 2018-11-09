fc374d656cde41a6a87687038349f66546e65f90
Kordell Stewart <30807917+MiltonMan@users.noreply.github.com>
2018-11-08 21:52:32 -0500

    Added hiding/reveal and groundwork

--- incognito.c
+++ incognito.c
@@ -8,54 +8,6 @@
 #include <linux/kobject.h>
 #include <linux/syscalls.h>
 #include <linux/slab.h>
-
-
-MODULE_LICENSE("GPL");
-
-int incognito_init(void);
-void incognito_exit(void);
-module_init(incognito_init);
-module_exit(incognito_exit);
-
-#ifndef __KERNEL__
-#define __KERNEL__
-#endif
-
-#if defined(__i386__)
-#define START_CHECK 0xc0000000
-#define END_CHECK 0xd0000000
-typedef unsigned int psize;
-#else
-#define START_CHECK 0xffffffff81000000
-#define END_CHECK 0xffffffffa2000000
-typedef unsigned long psize;
-#endif
-
-psize *sys_call_table;
-psize **find(void) {
- psize **sctable;
- psize i = START_CHECK;
- while (i < END_CHECK) {
-  sctable = (psize **) i;
-  if (sctable[__NR_close] == (psize *) sys_close) {
-   return &sctable[0];
-  }
-  i += sizeof(void *);
- }
- return NULL;
-}
-
-
-int __init incognito_init(void) {#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/sched.h> // task_struct definition
-#include <asm/unistd.h>
-#include <linux/list.h>
-#include <linux/init_task.h>
-#include <linux/kobject.h>
-#include <linux/syscalls.h>
-#include <linux/slab.h>
 #include <linux/version.h>
 
 #include <asm/uaccess.h> //we are dealing with most recent kernal
