898ee9b831abcf4b1ed1d0ccc1223fbaad894376
Kordell Stewart <30807917+MiltonMan@users.noreply.github.com>
2018-11-15 00:31:45 -0500

    Delete .incognito.ko.cmd

--- .incognito.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/adhas980/Desktop/Incognito/incognito.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/adhas980/Desktop/Incognito/incognito.ko /home/adhas980/Desktop/Incognito/incognito.o /home/adhas980/Desktop/Incognito/incognito.mod.o ;  true
